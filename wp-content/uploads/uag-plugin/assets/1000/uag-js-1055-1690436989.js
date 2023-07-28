f2a9a9c75f65c37667fa330d9b14dd49afde93e6
Suwarto <rossisuwar@gmail.com>
2023-07-28 12:40:51 +0700

    desatanjungmenang

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1055-1690436989.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"c96613c5","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-c96613c5', 1055 );
+});
+ });
